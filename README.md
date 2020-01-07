7276cc46f04fabbed1d41ef81870a3e51716521d
Kris Gesling <krisgesling@gmail.com>
2020-01-07 17:25:44 +0930

    initial commit

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/microphone-alt.svg" card_color="#CCCCCC" width="50" height="50" style="vertical-align:bottom"/> End Listening Sound
-
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/microphone-alt-slash.svg' card_color='#333333' width='50' height='50' style='vertical-align:bottom'/> End Listening Sound
+Play sound when microphone stops listening
 
 ## About
+This enables support for the `end_listening` configuration parameter, playing a sound at the end of the microphone recording.
 
+By default it will attempt to play the file: `mycroft-core/mycroft/res/snd/end_listening.wav`. If the file isn't found it will play the standard `acknowledge.mp3`.
 
 ## Examples
+* "<All phrases>"
 
 ## Credits
 krisgesling
@@ -14,4 +17,4 @@ krisgesling
 
 ## Tags
 #System
-
+#Microphone
--- __init__.py
+++ __init__.py
@@ -1,15 +1,21 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util import play_audio_file, resolve_resource_file
 
 
 class EndListeningSound(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+    def initialize(self):
+        self.sound = resolve_resource_file(
+            self.config_core.get("sounds").get("end_listening")
+        )
+        self.log.info(self.sound)
+        self.add_event("recognizer_loop:record_end", self.handle_record_end)
 
-    @intent_file_handler('sound.listening.end.intent')
-    def handle_sound_listening_end(self, message):
-        self.speak_dialog('sound.listening.end')
+    def handle_record_end(self, message):
+        try:
+            play_audio_file(self.sound)
+        except:
+            play_audio_file(resolve_resource_file("snd/acknowledge.mp3"))
 
 
 def create_skill():
     return EndListeningSound()
-
